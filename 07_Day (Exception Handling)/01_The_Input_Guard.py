7bd3587e080d04c58a08620c279b29e53e9a886a
Sourav paul <souravp3123@gmail.com>
2025-12-21 21:41:47 +0600

    Delete 07_Day (Exception Handling) directory

--- 07_Day (Exception Handling)/01_The_Input_Guard.py
+++
@@ -1,9 +0,0 @@
-
-while True:
-    try:
-        age=int(input('Enter your age :'))
-        print('Age :',age)
-        break
-    except ValueError:
-        print('Text is not allowed.')
-    
--- 07_Day (Exception Handling)/02_The_Math_Safety_Net.py
+++
@@ -1,5 +0,0 @@
-x=0
-try :
-    print(100/x)
-except ZeroDivisionError:
-    print('Not possible to divide by zero.')
